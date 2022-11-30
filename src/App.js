c51af42b51e9470acca067a941f841fd95cfdd59
leilafaez <leila.faez@gmail.com>
2022-11-30 17:22:54 +0000

    Complete exercise F, G

--- src/App.js
+++ src/App.js
@@ -5,25 +5,55 @@ function App(){
     <div>
     <Logo />
     <BestPokemon />
+    <CaughtPokemon />
     </div>
   );
 }
-
-function Logo(){
+const Logo = ()=>{
+  const appName = "Leila Pokedex";
   return (
+
     <header>
-      <h1>Welcome to the Pokedex</h1>;
+      <h1>Welcome to the {appName}</h1>;
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt=""
       ></img>
     </header>
   );
+}
+const CaughtPokemon= ()=>{
+  const date = new Date().toLocaleDateString();
+  return <p>Caught 0 Pokemon on {date}</p>;
+}
+// function Logo(){
+//   return (
+//     <header>
+//       <h1>Welcome to the Pokedex</h1>;
+//       <img
+//         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+//         alt=""
+//       ></img>
+//     </header>
+//   );
   
+// }
+const BestPokemon = ()=>{
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+  return (
+    <div>
+      <p>My favorite Pokemon is Squirtle</p>
+      <ul>
+        {abilities.map((ability) => (
+          <li>{ability}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-function BestPokemon(){
-  return (<p>My favorite Pokemon is Squirtle</p>);
+// function BestPokemon(){
+//   return (<p>My favorite Pokemon is Squirtle</p>);
   
-}
+// }
 export default App;
