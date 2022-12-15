f13f6d49a7a244e7d388f2e497973b854eef4146
leilafaez <leila.faez@gmail.com>
2022-12-15 15:39:22 +0000

    Done exercise of lesson 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
+import PokemonMovesSelector from "./PokemonMovesSelector";
+import PokemonCity from "./PokemonCity";
 
 function App(){
   return (
@@ -9,6 +11,8 @@ function App(){
       <Logo appName="Pokedex" handleClick={logWhenClicked}/>
       <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]} />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
+      <PokemonMovesSelector />
+      <PokemonCity/>
     </div>
   );
 }
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -15,21 +15,31 @@ const CaughtPokemon = (props) => {
 //     <button onClick={catchPokemon}>click</button>
 //   </div>;
   const [caught, setCaught] = useState([]);
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
   //  const date = new Date().toLocaleDateString();
   const catchPokemon = () => {
-    return setCaught(caught.concat(Math.floor(Math.random() * caught.length)));
+    return setCaught(caught.concat(Math.floor(Math.random() *caught.length ) + " "+pokemonNameInput))
+    
     
   };
+  
+
+  function handleInputChange(event){
+    setPokemonNameInput(event.target.value);
+   
+  }
+
   return (
     <div>
       <p>
         Caught {caught.length} Pokemon on {props.date}
       </p>
       <ul>
-        {caught.map((element,index)=>{
-          return <li key={index}>{element}</li>
+        {caught.map((element, index) => {
+          return <li key={index}>{element}</li>;
         })}
       </ul>
+      <input type="text" value={pokemonNameInput} onChange={handleInputChange} />
       <button onClick={catchPokemon}>click</button>
     </div>
   );
